18d02aa37d1398e85142a6bf696ab46f849b80f8
AlbaOrbegozo <albaorbegozo1@gmail.com>
2021-05-02 21:32:43 +0200

    Añado el xml y clases de eventos y de evento.

---
+++ app/src/main/java/com/politecnico/materchef_pmdm_albajonathan/CustomAdapter.java
@@ -0,0 +1,72 @@
+package com.politecnico.materchef_pmdm_albajonathan;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    ArrayList<String> nombre;
+    ArrayList<String> direccion;
+    ArrayList<String> localidad;
+    ArrayList<String> telefono;
+    Context context;
+
+    public CustomAdapter(Context context, ArrayList<String> nombre, ArrayList<String> direccion, ArrayList<String> localidad, ArrayList<String> telefono) {
+        this.context = context;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.localidad = localidad;
+        this.telefono = telefono;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // inflate the item Layout
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rowlayout, parent, false);
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // set the data in items
+        holder.nombre.setText(nombre.get(position));
+        holder.direccion.setText(direccion.get(position));
+        holder.localidad.setText(localidad.get(position));
+        holder.telefono.setText(telefono.get(position));
+        // implement setOnClickListener event on item view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // display a toast with person name on item click
+                Toast.makeText(context, telefono.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return nombre.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView nombre, direccion, localidad , telefono;// init the item view's
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            // get the reference of item view's
+            nombre = (TextView) itemView.findViewById(R.id.nombre);
+            direccion = (TextView) itemView.findViewById(R.id.direccion);
+            localidad = (TextView) itemView.findViewById(R.id.localidad);
+            telefono =  (TextView) itemView.findViewById(R.id.telefono);
+        }
+    }
+}
+
---
+++ app/src/main/java/com/politecnico/materchef_pmdm_albajonathan/EventosActivity.java
@@ -0,0 +1,72 @@
+package com.politecnico.materchef_pmdm_albajonathan;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class EventosActivity extends AppCompatActivity  {
+
+    // ArrayList for person names, email Id's and mobile numbers
+    ArrayList<String> nombre = new ArrayList<>();
+    ArrayList<String> direccion = new ArrayList<>();
+    ArrayList<String> localidad = new ArrayList<>();
+    ArrayList<String> telefono = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventos);
+        // get the reference of RecyclerView
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        // set a LinearLayoutManager with default vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        try {
+            // get JSONObject from JSON file
+            JSONObject obj = new JSONObject(loadJSONFromAsset());
+            // fetch JSONArray named users
+            JSONArray userArray = obj.getJSONArray("ITEMS");
+            // implement for loop for getting users list data
+            for (int i = 0; i < userArray.length(); i++) {
+                // create a JSONObject for fetching single user data
+                JSONObject userDetail = userArray.getJSONObject(i);
+                // fetch email and name and store it in arraylist
+                nombre.add(userDetail.getString("NOMBRE"));
+                direccion.add(userDetail.getString("DIRECCION"));
+                localidad.add(userDetail.getString("LOCALIDAD"));
+                telefono.add(userDetail.getString("TELEFONO"));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //  call the constructor of CustomAdapter to send the reference and data to Adapter
+        CustomAdapter customAdapter = new CustomAdapter(EventosActivity.this, nombre, direccion, localidad , telefono);
+        recyclerView.setAdapter(customAdapter); // set the Adapter to RecyclerView
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("CentrosSanitarios.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+}
