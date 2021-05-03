5a31cf7578d3cf9353fcaa04c92a27a257f9b218
Jonathan <989nuzumaki@gmail.com>
2021-05-03 18:48:43 +0200

    prueba links activitys

--- app/src/main/java/com/politecnico/masterchef_pmdm_albajonathan/LoginActivity.java
+++ app/src/main/java/com/politecnico/masterchef_pmdm_albajonathan/LoginActivity.java
@@ -1,15 +1,40 @@
 package com.politecnico.masterchef_pmdm_albajonathan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.politecnico.materchef_pmdm_albajonathan.R;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btnInicioSesion;
+    Button btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        btnInicioSesion = (Button) findViewById(R.id.btnIniciarSesion);
+        btnInicioSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this , EventosActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegistrarse = (Button) findViewById(R.id.btnRegistrarse);
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this , RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
